1aee9b587dbd781219786e608bfdd8259f3dd1ce
Tomas Blanda <tomasblanda@gmail.com>
2022-09-08 20:54:54 +0200

    Added searching.

--- chatterbox/views.py
+++ chatterbox/views.py
@@ -3,5 +3,23 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from chatterbox.models import Room, Message
+
+
 def hello(request, s):
     return HttpResponse(f'Hello, {s} world!!!')
+
+
+def search(request, s):
+    rooms = Room.objects.filter(name__contains=s)
+    # response = "Rooms: "
+    # for room in rooms:
+    #     response += room.name + ", "
+
+    messages = Message.objects.filter(body__contains=s)
+    # response += "<br>Message: "
+    # for message in messages:
+    #     response += message.body[0:10] + "... ,"
+
+    context = {'rooms': rooms, 'messages': messages}
+    return render(request, "chatterbox/search.html", context)
